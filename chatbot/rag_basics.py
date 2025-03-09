ab2e237dbce64033689d833492882de08ae9efcf
Poorvi <poorvi.b26@gmail.com>
2025-03-08 18:55:54 -0800

    add api for chatbot by defining a post route

--- chatbot/rag_basics.py
+++ chatbot/rag_basics.py
@@ -7,7 +7,12 @@ from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from vectorstore import create_embeddings
+from flask import Flask, jsonify, request
 import os
+
+VECTORSTORE_DIR = "./chroma_db"
+
+app = Flask(__name__)
 
 load_dotenv()
 
@@ -24,11 +29,6 @@ docs = textsplitter.split_documents(all_docs)
 
 vectorstore = create_embeddings(docs)
 retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 10})
-
-# embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-
-# vectorstore = Chroma.from_documents(documents=docs, embedding=GoogleGenerativeAIEmbeddings(model="models/embedding-001"))
-# retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 10})
 
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.4, max_tokens=500)
 
@@ -53,16 +53,45 @@ prompt = ChatPromptTemplate.from_messages(
 question_answer_chain = create_stuff_documents_chain(llm, prompt)
 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
-while True:
-    question = input("Enter your question: ")
-    if question == "exit":
-        break
-    response = rag_chain.invoke({"input":question})
-    print(response["answer"])
-    print("\n")
 
 # question = "Hello how are you doing? Are there any communities in BC or Canada for trans people? If yes, provide me with some...\n"
 # print(question)
 
 # response = rag_chain.invoke({"input":question})
 # print(response["answer"])
+
+def load_embeddings():
+    if os.path.exists(VECTORSTORE_DIR):
+        vectorstore = Chroma(persist_directory=VECTORSTORE_DIR, embedding_function=GoogleGenerativeAIEmbeddings(model="models/embedding-001"))
+        print(f"Embeddings loaded from {VECTORSTORE_DIR}")
+        return vectorstore
+    else:
+        print(f"No embeddings found in {VECTORSTORE_DIR}, run the save_embeddings function first.")
+        return None
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    question = data.get("question")
+    vectorstore = load_embeddings()
+    if question == "exit":
+        return
+    if vectorstore:
+        response = rag_chain.invoke({"input":question})
+        return jsonify({"answer": f'{response["answer"]} \n'})
+    else:
+        return jsonify({"error": "No embeddings found in the directory"}), 400
+
+'''
+Sample request:
+{
+    "question": "Hello! Please give me some mental health reources in Canada, particularly in BC"
+}
+Sameple response:
+{
+    "answer": "It's great that you're reaching out for support! In BC, you can access the Ministry of Mental Health and Addictions' \"Help Starts Here\" website. It offers a navigation tool to find mental health and substance use resources and services, plus information on well-being.  The website is in English, with translations available in over 100 languages. Nationally, the Canadian Mental Health Association (CMHA) provides information on mental health resources, programs, and support.  You can also find your local CMHA branch through their website.  Remember, reaching out is a sign of strength, and I'm here to help you on your journey.  If you need immediate support, please call or text 9-8-8, Canada's Suicide Crisis Helpline. \n"
+}
+'''
+
+if __name__ == '__main__':
+    app.run(debug=True)
